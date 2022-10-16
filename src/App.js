3cbc21f9ed7656d7450f876d7372f80ff987b4ed
RyfiMagicman <ryfigames@gmail.com>
2022-10-16 16:02:30 -0400

    banner on homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import "./App.css";
 import { Header } from "./components";
+import bannerImg from "./homeBanner.jpeg";
+import Card from "react-bootstrap/Card";
 
 function App() {
-  return <>dip</>;
+  return (
+    <>
+      <div
+        style={{
+          height: "400px",
+          backgroundImage: `url(${bannerImg})`,
+          backgroundSize: "contain",
+        }}
+      >
+        big chungus
+      </div>
+      <h1 className="text-center fw-bold">Featured Games</h1>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          {/* <Button variant="primary">Go somewhere</Button> */}
+        </Card.Body>
+      </Card>
+    </>
+  );
 }
 
 export default App;
